4fb216d85b8667335f9ba9330be60f8fed20ec64
xxy8nq <xxy8nq@virginia.edu>
2026-02-19 12:58:03 -0500

    Graphing Cases over Time

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -4,7 +4,16 @@ import matplotlib.pyplot as plt
 
 #%%
 # Load the data
-data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+data = pd.read_csv("Data/mystery_virus_daily_active_counts_RELEASE#1.csv", parse_dates=['date'], header=0, index_col=None)
 
 #%%
 # Make a plot of the active cases over time
+plt.figure(figsize=(10, 6))
+plt.plot(data['date'], data['active reported daily cases'], marker='o', linestyle='-')
+plt.title('Daily Active Cases of the Mystery Virus')
+plt.xlabel('Date')
+plt.ylabel('Number of Active Cases')
+plt.xticks(rotation=45)
+plt.grid()
+plt.tight_layout()
+plt.show()
